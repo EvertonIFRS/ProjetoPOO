64919f78bfe6b62e04f8083742d2d6bbca5ebd83
05180176 <05180176@D54992.farroupilha.ifrs.edu.br>
2019-04-01 16:24:58 -0300

    Teste

--- Eficiencia/src/eficiencia/FXML_LoginController.java
+++ Eficiencia/src/eficiencia/FXML_LoginController.java
@@ -30,7 +30,7 @@ public class FXML_LoginController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }
 
     @FXML

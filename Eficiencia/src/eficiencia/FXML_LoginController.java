b7d931e3adc17c86ff3a4983138d11adf6b931a4
05180176 <05180176@D54992.farroupilha.ifrs.edu.br>
2019-04-01 16:25:44 -0300

    Janela de Login.

--- Eficiencia/src/eficiencia/FXML_LoginController.java
+++ Eficiencia/src/eficiencia/FXML_LoginController.java
@@ -27,6 +27,8 @@ public class FXML_LoginController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {

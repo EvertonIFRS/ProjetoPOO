cec8735e182e8a81c7cfaee19eafc46d65360aa1
05180176 <05180176@D54992.farroupilha.ifrs.edu.br>
2019-04-01 16:22:24 -0300

    Janela de Login.

---
+++ Eficiencia/src/eficiencia/FXML_LoginController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eficiencia;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author 05180176
+ */
+public class FXML_LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    public void FuncaoRegistro(ActionEvent e) {
+    }
+
+    @FXML
+    public void FuncaoCheck(ActionEvent e) {
+
+        try {
+            Parent root;
+            root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            stage.setOnCloseRequest(ee -> {
+                stage.hide();
+            });
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+
+        }
+
+    }
+
+}
